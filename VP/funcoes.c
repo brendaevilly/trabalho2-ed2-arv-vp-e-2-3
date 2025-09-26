c4e3a1851cb6d055abc8c5447519cdf9369829be
callmepeh <pedrosasada@gmail.com>
2025-09-26 15:34:21 -0300

    Adição da declaração das funções no prototipo.h da rubroNegra e da funções de balanceamento da mesma

--- VP/funcoes.c
+++ VP/funcoes.c
@@ -2,3 +2,18 @@
 #include <stdlib.h>
 #include "prototipos.h"
 
+
+//funções básicas da arvore rubro-negra (aqui criamos os nós, rotacionamos etc)
+
+Arvore* criarNo (TipoDado tipo, DadoUnion dado) {
+    Arvore* novoNo = (Arvore*)malloc(sizeof(Arvore));
+    if (!novoNo) {
+        fprintf(stderr, "Erro ao alocar memoria para novo no.\n");
+        exit(EXIT_FAILURE);
+    }
+    novoNo->tipo = tipo;
+    novoNo->dado = dado;
+    novoNo->cor = VERMELHO; 
+    novoNo->esq = novoNo->dir = NULL;
+    return novoNo;
+}
--- VP/prototipos.h
+++ VP/prototipos.h
@@ -41,4 +41,72 @@ typedef struct arvore{
     struct arvore *esq, *dir;
 }Arvore;
 
+// operações básicas da arvore rubro-negra
+
+// Criação e manipulação de nós
+Arvore* criarNo(TipoDado tipo, DadoUnion dado);
+void liberarArvore(Arvore* raiz);
+// rotações
+Arvore* rotacaoEsquerda(Arvore** h);
+Arvore* rotacaoDireita(Arvore **h);
+// Operações de cor
+void trocarCores(Arvore **h);
+int ehVermelho(Arvore* no);
+// Inserção e remoção
+Arvore* inserirRB(Arvore **h, TipoDado tipo, DadoUnion dado, char* nome);
+Arvore* removerRB(Arvore **h, char* nome, TipoDado tipo);
+
+
+//operações de busca
+
+// Busca de artistas
+Arvore* buscarArtista(Arvore* raiz, char* nome);
+void buscarEExibirArtista(Arvore* raiz);
+// Busca de álbuns
+Arvore* buscarAlbum(Arvore* raizAlbuns, char* titulo);
+void buscarEExibirAlbum(Arvore* raiz);
+// Busca de músicas
+Musica* buscarMusica(Musica* head, char* titulo);
+void buscarMusicaGlobal(Arvore* raiz, char* tituloMusica);
+void buscarMusicaEmAlbuns(Arvore* albumRaiz, char* tituloMusica, char* nomeArtista);
+//operações de inserção
+// Inserção de artistas
+Arvore* inserirArtista(Arvore* raiz);
+Arvore* inserirArtistaManual(Arvore* raiz, char* nome, TipoArtista tipo, char* estilo);
+// Inserção de álbuns
+void inserirAlbum(Arvore* raiz);
+int inserirAlbumManual(Arvore* raiz, char* nomeArtista, char* tituloAlbum, int ano);
+// Inserção de músicas
+void inserirMusicaNoAlbum(Arvore* raiz);
+int inserirMusicaManual(Arvore* raiz, char* nomeArtista, char* tituloAlbum, char* tituloMusica, int minutos);
+Musica* inserirMusica(Musica* head, char* titulo, int minutos);
+
+
+//operações de remoção
+
+Arvore* removerArtista(Arvore* raiz);
+void liberarArtista(Artista* artista);
+// Remoção de álbuns
+void removerAlbum(Arvore* raiz);
+void liberarAlbum(Album* album);
+// Remoção de músicas
+void removerMusica(Arvore* raiz);
+Musica* removerMusicaDaLista(Musica* head, char* titulo);
+void liberarListaMusicas(Musica* head);
+
+
+//operações de exibição
+
+void listarArtistas(Arvore* raiz);
+void listarArtistasDetalhado(Arvore* raiz);
+void exibirArtista(Artista* artista);
+// Listagem de álbuns
+void listarAlbunsArtista(Arvore* albumRaiz);
+void listarAlbunsDetalhado(Arvore* albumRaiz);
+void exibirAlbum(Album* album);
+// Listagem de músicas
+void listarMusicas(Musica* head);
+void listarMusicasDetalhado(Musica* head);
+void exibirMusica(Musica* musica);
+
 #endif
